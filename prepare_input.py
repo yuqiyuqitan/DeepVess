aa3fa8cbf76ae700a4eb5b9d75bd29cba1ff3b4c
yuqiyuqitan <tanyuqi527@gmail.com>
2021-10-02 15:54:37 -0400

    update prepare_input.py

--- prepare_input.py
+++ prepare_input.py
@@ -7,12 +7,12 @@ import os
 import argparse
 
 #load tiff file or czi file to python
-def prepare_input(input_fname, output_fname, output_dir):
+def prepare_input(input_fname, output_fname, output_dir="."):
 	"""
 	"""
 	im = io.imread(input_fname)
 
-	#remove the top and bottom percent of the pixel per slice
+	#remove the top and bottom percent of the pixel
 	#normalize the data from -0.5 to 0.5
 	p_min, p_max = np.percentile(im, (1,98))
 	im_new = exposure.rescale_intensity(im, in_range = (p_min, p_max), 
@@ -21,7 +21,7 @@ def prepare_input(input_fname, output_fname, output_dir):
 
 	#save as h5 formate 
 	save_path = os.path.join(output_dir, output_fname) + '.hdf5'
-	hf = h5py.File(save_path, 'a')
+	hf = h5py.File(save_path, 'a')ls
 	dset = hf.create_dataset('im', data=im_new)
 	hf.close()
 
@@ -29,6 +29,6 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "prepare data for deepVess")
 	parser.add_argument('input_fname', type = str)
 	parser.add_argument('output_fname', type = str)
-	parser.add_argument('output_dir', type = str)
+	parser.add_argument('--output_dir', type = str,default='.')
 	args = parser.parse_args()
 	prepare_input(args.input_fname, args.output_fname, args.output_dir)
